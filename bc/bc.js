2e4e6ef97799ed3af54be8392e0250bc1562b62c
Prathap Natarajan <pnatarajan@constantcontact.com>
2012-05-30 16:38:04 -0700

    beat me again

--- bc/bc.js
+++ bc/bc.js
@@ -1,3 +1,5 @@
 (function() {
-    document.getElementsByClassName("leaderboard")[0].innerHTML='<div class="pull-right leaderboardUnit">&infin;</div><img class="grav30" data-gravatar="ad049fd936f8a51088aaa27bf74157ea" src="https://secure.gravatar.com/avatar/ad049fd936f8a51088aaa27bf74157ea?s=30&amp;d=https%3A%2F%2Frobohash.org%2Fad049fd936f8a51088aaa27bf74157ea%3Fsize%3D30x30%26bgset%3Dany"> <strong>Beat Me!</strong>';
+    n = document.getElementsByClassName("leaderboard")[0];
+    n.innerHTML='<div class="pull-right leaderboardUnit">&infin;</div><img class="grav30" data-gravatar="ad049fd936f8a51088aaa27bf74157ea" src="https://secure.gravatar.com/avatar/ad049fd936f8a51088aaa27bf74157ea?s=30&amp;d=https%3A%2F%2Frobohash.org%2Fad049fd936f8a51088aaa27bf74157ea%3Fsize%3D30x30%26bgset%3Dany"> <strong>Beat Me!</strong>';
+    n.removeAttribute("class");
 }).call(this);
